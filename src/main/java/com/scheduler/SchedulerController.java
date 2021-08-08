7db579d7ee043373dc7548a8693f341e2df70842
ondracekl <lukas.ondracek@ice.cz>
2021-08-08 13:39:03 +0200

    Added addEvent

--- src/main/java/com/scheduler/SchedulerController.java
+++ src/main/java/com/scheduler/SchedulerController.java
@@ -1,17 +1,23 @@
 package com.scheduler;
 
 import com.scheduler.model.User;
+import com.scheduler.model.Event;
 import com.scheduler.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.scheduler.repository.EventRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SchedulerController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
 
-    @PostMapping("/add")
+    public SchedulerController(UserRepository userRepository, EventRepository eventRepository) {
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
+    }
+
+    @PostMapping("/addUser")
     public String addUser(@RequestParam String first, @RequestParam String last) {
         User user = new User();
         user.setFirstName(first);
@@ -28,5 +34,19 @@ public class SchedulerController {
     @GetMapping("/find/{id}")
     public User findUserById(@PathVariable Integer id) {
         return userRepository.findUserById(id);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @PostMapping("/addEvent")
+    public String addEvent(@RequestParam String name, @RequestParam String description) {
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+        eventRepository.save(event);
+        return "Added new event to repo!";
     }
 }
