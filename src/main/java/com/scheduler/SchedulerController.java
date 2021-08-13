dde701d818366bf6ffd1b04c19a8d9a015e07f8c
ondracekl <lukas.ondracek@ice.cz>
2021-08-13 10:24:01 +0200

    Thymeleaf

--- src/main/java/com/scheduler/SchedulerController.java
+++ src/main/java/com/scheduler/SchedulerController.java
@@ -4,9 +4,15 @@ import com.scheduler.model.User;
 import com.scheduler.model.Event;
 import com.scheduler.repository.UserRepository;
 import com.scheduler.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class SchedulerController {
 
     private final UserRepository userRepository;
@@ -17,11 +23,35 @@ public class SchedulerController {
         this.eventRepository = eventRepository;
     }
 
+    @GetMapping("/")
+    public String main(Model model) {
+        return "main";
+    }
+
+    @GetMapping("/newUser")
+    public String newUser(Model model) {
+        return "newUser";
+    }
+
+    // /hello?name=jmeno
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+                    String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "main"; //view
+    }
+
     @PostMapping("/addUser")
     public String addUser(@RequestParam String first, @RequestParam String last, @RequestParam String password,
-                          @RequestParam String email, @RequestParam String phone) {
-        if (first == "" || last == "" || password == "" || email == "" || phone == "")
-            return "Vyplňte prosím všechna pole.";
+                          @RequestParam String email, @RequestParam String phone, Model model) {
+        if (first == "" || last == "" || password == "" || email == "" || phone == "") {
+            model.addAttribute("message", "Vyplňte prosím všechna pole.");
+            return "message";
+        }
+
         User user = new User();
         user.setFirstName(first);
         user.setLastName(last);
@@ -29,12 +59,14 @@ public class SchedulerController {
         user.setEmail(email);
         user.setPhone(phone);
         userRepository.save(user);
-        return "Registrace proběhla úspěšně.";
+        model.addAttribute("message", "Registrace proběhla úspěšně.");
+        return "message";
     }
 
     @GetMapping("/list")
-    public Iterable<User> getUsers() {
-        return userRepository.findAll();
+    public String getUsers(Model model) {
+        model.addAttribute("message", userRepository.findAll());
+        return "table";
     }
 
     @GetMapping("/find/{id}")
@@ -42,12 +74,18 @@ public class SchedulerController {
         return userRepository.findUserById(id);
     }
 
+    @GetMapping("/newEvent")
+    public String newEvent(Model model) {
+        return "newEvent";
+    }
+
     @PostMapping("/addEvent")
-    public String addEvent(@RequestParam String name, @RequestParam String description) {
+    public String addEvent(@RequestParam String name, @RequestParam String description, Model model) {
         Event event = new Event();
         event.setName(name);
         event.setDescription(description);
         eventRepository.save(event);
-        return "Nová událost vytvořena, přidejte termíny a místa";
+        model.addAttribute("message", "Nová událost vytvořena, přidejte termíny a místa.");
+        return "message";
     }
 }
