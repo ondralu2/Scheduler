c5686164d664b7dc4e6312943c2b44c89da461cf
ondracekl <lukas.ondracek@ice.cz>
2022-01-01 20:54:24 +0100

    Nice formatting - country, latitude and longitude direction

--- src/main/java/com/project/scheduler/entity/LatitudeDirection.java
+++ src/main/java/com/project/scheduler/entity/LatitudeDirection.java
@@ -1,6 +1,17 @@
 package com.project.scheduler.entity;
 
 public enum LatitudeDirection {
-    N,
-    S
+    N("S"),
+    S("J");
+
+    private final String displayValue;
+
+    private LatitudeDirection(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
 }
--- src/main/java/com/project/scheduler/entity/LongitudeDirection.java
+++ src/main/java/com/project/scheduler/entity/LongitudeDirection.java
@@ -1,6 +1,16 @@
 package com.project.scheduler.entity;
 
 public enum LongitudeDirection {
-    E,
-    W
+    E("V"),
+    W("Z");
+
+    private final String displayValue;
+
+    private LongitudeDirection(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
